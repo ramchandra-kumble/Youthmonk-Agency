4cc0da1b722beb5e3e869dd30bc93380616c0c09
vomi-exe <mehtavomi@gmail.com>
2021-05-22 15:04:49 +0530

    webdev page added  and sidebar chnages

--- assets/js/main.js
+++ assets/js/main.js
@@ -79,7 +79,7 @@
 			});
 
 			breakpoints.on('>large', function() {
-				$sidebar.removeClass('inactive');
+				$sidebar.addClass('inactive');
 			});
 
 		// Hack: Workaround for Chrome/Android scrollbar position bug.
