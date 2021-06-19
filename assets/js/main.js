3d8b25fc45bf97d8a0d76d717ea2d0dd4d2ef34c
Kshantu Syon <83629287+syon001@users.noreply.github.com>
2021-06-19 10:22:55 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,26 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
+/*countdown-timer*/
+var count = new Date("jan 1,2022 00:01:00").getTime();
+  var x = setInterval(function() {
+    var now = new Date().getTime();
+    var d = count - now;
+
+    var days = Math.floor(d/(1000*60*60*24));
+    var hours = Math.floor((d%(1000*60*60*24))/(1000*60*60));
+    var minutes = Math.floor((d%(1000*60*60))/(1000*60));
+    var seconds = Math.floor((d%(1000*60))/1000);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    if(d <= 0){
+      clearInterval(x);
+    }
+  },1000);
 
 (function($) {
 
@@ -296,22 +316,4 @@ document.querySelector(".submit").addEventListener('click', function(e) {
 
 
 
-var count = new Date("jan 1,2022 00:01:00").getTime();
-  var x = setInterval(function() {
-    var now = new Date().getTime();
-    var d = count - now;
 
-    var days = Math.floor(d/(1000*60*60*24));
-    var hours = Math.floor((d%(1000*60*60*24))/(1000*60*60));
-    var minutes = Math.floor((d%(1000*60*60))/(1000*60));
-    var seconds = Math.floor((d%(1000*60))/1000);
-
-    document.getElementById("days").innerHTML = days;
-    document.getElementById("hours").innerHTML = hours;
-    document.getElementById("minutes").innerHTML = minutes;
-    document.getElementById("seconds").innerHTML = seconds;
-
-    if(d <= 0){
-      clearInterval(x);
-    }
-  },1000);
